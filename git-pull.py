efacda8091de6c6885ff071ef4e1f84bfc936dad
Bonkeybee <prs100791@gmail.com>
2021-03-02 12:26:40 -0600

    update addon when pulling

--- git-pull.py
+++ git-pull.py
@@ -1,3 +1,16 @@
 import subprocess
-output = subprocess.check_output(["git", "pull"])
-print(output)
+print(subprocess.check_output(["git", "pull"]))
+
+import configparser
+config = configparser.ConfigParser()
+config.read('config.ini')
+ADDON_PATH = config['wow']['ADDON_PATH']
+
+import shutil
+
+
+BACK_SLASH = '\\'
+GUILDCHATLOGGER_ADDON = 'GuildChatLogger'
+
+shutil.rmtree(ADDON_PATH + BACK_SLASH + GUILDCHATLOGGER_ADDON, True)
+shutil.copytree(GUILDCHATLOGGER_ADDON, ADDON_PATH + BACK_SLASH + GUILDCHATLOGGER_ADDON)

833294a288f04514b8cc7d4f5ddf982b5b9e1867
Patrick <prs100791@gmail.com>
2022-01-01 00:23:23 -0600

    add phonetic item/spell/talent replacements

--- lib/beans/message.py
+++ lib/beans/message.py
@@ -17,8 +17,8 @@ class Message:
     def __init__(self, timestamp, player, line):
         self.timestamp = timestamp
         self.player = player.encode("UTF-8", "ignore").decode("UTF-8", "ignore")
-        self.line = replace_talent_patterns(replace_spell_patterns(replace_enchant_patterns(replace_item_patterns(replace_raidmarks(replace_mentions(replace_escape_sequences(line))))))).encode("UTF-8", "ignore").decode("UTF-8", "ignore")
-        self.raw = line
+        self.line = replace_talent_patterns(replace_spell_patterns(replace_enchant_patterns(replace_item_patterns(replace_raidmarks(replace_mentions(replace_escape_sequences(line)), False), False), False), False), False).encode("UTF-8", "ignore").decode("UTF-8", "ignore")
+        self.raw = replace_talent_patterns(replace_spell_patterns(replace_enchant_patterns(replace_item_patterns(replace_raidmarks(line, True), True), True), True), True).encode("UTF-8", "ignore").decode("UTF-8", "ignore")
 
     def __lt__(self, other) -> bool:
         if type(other) is Message:
@@ -72,54 +72,90 @@ def replace_mentions(line: str) -> str:
     return line
 
 
-def replace_raidmarks(line: str) -> str:
+def replace_raidmarks(line: str, phonetic: bool) -> str:
     """Replaces World of Warcraft raid markers found in the string with corresponding Discord emojis"""
     debug = 'Mutating(raidmarks): '
     if constants.SKULL in line:
         LOG.debug(debug + line)
-        line = line.replace(constants.SKULL, settings.load()['discord']['skull_emoji'])
+        if phonetic:
+            line = line.replace(constants.SKULL, "skull")
+        else:
+            line = line.replace(constants.SKULL, settings.load()['discord']['skull_emoji'])
     if constants.CROSS in line:
         LOG.debug(debug + line)
-        line = line.replace(constants.CROSS, settings.load()['discord']['cross_emoji'])
+        if phonetic:
+            line = line.replace(constants.CROSS, "cross")
+        else:
+            line = line.replace(constants.CROSS, settings.load()['discord']['cross_emoji'])
     if constants.SQUARE in line:
         LOG.debug(debug + line)
-        line = line.replace(constants.SQUARE, settings.load()['discord']['square_emoji'])
+        if phonetic:
+            line = line.replace(constants.SQUARE, "square")
+        else:
+            line = line.replace(constants.SQUARE, settings.load()['discord']['square_emoji'])
     if constants.MOON in line:
         LOG.debug(debug + line)
-        line = line.replace(constants.MOON, settings.load()['discord']['moon_emoji'])
+        if phonetic:
+            line = line.replace(constants.MOON, "moon")
+        else:
+            line = line.replace(constants.MOON, settings.load()['discord']['moon_emoji'])
     if constants.TRIANGLE in line:
         LOG.debug(debug + line)
-        line = line.replace(constants.TRIANGLE, settings.load()['discord']['triangle_emoji'])
+        if phonetic:
+            line = line.replace(constants.TRIANGLE, "triangle")
+        else:
+            line = line.replace(constants.TRIANGLE, settings.load()['discord']['triangle_emoji'])
     if constants.DIAMOND in line:
         LOG.debug(debug + line)
-        line = line.replace(constants.DIAMOND, settings.load()['discord']['diamond_emoji'])
+        if phonetic:
+            line = line.replace(constants.DIAMOND, "diamond")
+        else:
+            line = line.replace(constants.DIAMOND, settings.load()['discord']['diamond_emoji'])
     if constants.CIRCLE in line:
         LOG.debug(debug + line)
-        line = line.replace(constants.CIRCLE, settings.load()['discord']['circle_emoji'])
+        if phonetic:
+            line = line.replace(constants.CIRCLE, "circle")
+        else:
+            line = line.replace(constants.CIRCLE, settings.load()['discord']['circle_emoji'])
     if constants.STAR in line:
         LOG.debug(debug + line)
-        line = line.replace(constants.STAR, settings.load()['discord']['star_emoji'])
+        if phonetic:
+            line = line.replace(constants.STAR, "star")
+        else:
+            line = line.replace(constants.STAR, settings.load()['discord']['star_emoji'])
     return line
 
 
-def replace_item_patterns(line):
+def replace_item_patterns(line: str, phonetic: bool) -> str:
     """Replaces World of Warcraft item patterns with wowhead.com urls"""
-    return replace_pattern('Mutating(item_patterns): ', constants.ITEM_PATTERN, line, constants.ITEM_REPLACE_REGEX, constants.WOWHEAD_ITEM_URL)
+    if phonetic:
+        return replace_pattern('Mutating(item_patterns): ', constants.ITEM_PHONETIC_PATTERN, line, constants.ITEM_PHONETIC_REPLACE_REGEX, None)
+    else:
+        return replace_pattern('Mutating(item_patterns): ', constants.ITEM_PATTERN, line, constants.ITEM_REPLACE_REGEX, constants.WOWHEAD_ITEM_URL)
 
 
-def replace_enchant_patterns(line):
+def replace_enchant_patterns(line: str, phonetic: bool) -> str:
     """Replaces World of Warcraft enchant patterns with wowhead.com urls"""
-    return replace_pattern('Mutating(enchant_patterns): ', constants.ENCHANT_PATTERN, line, constants.ENCHANT_REPLACE_REGEX, constants.WOWHEAD_SPELL_URL)
+    if phonetic:
+        return replace_pattern('Mutating(enchant_patterns): ', constants.ENCHANT_PHONETIC_PATTERN, line, constants.ENCHANT_PHONETIC_REPLACE_REGEX, None)
+    else:
+        return replace_pattern('Mutating(enchant_patterns): ', constants.ENCHANT_PATTERN, line, constants.ENCHANT_REPLACE_REGEX, constants.WOWHEAD_SPELL_URL)
 
 
-def replace_spell_patterns(line):
+def replace_spell_patterns(line: str, phonetic: bool) -> str:
     """Replaces World of Warcraft enchant patterns with wowhead.com urls"""
-    return replace_pattern('Mutating(spell_patterns): ', constants.SPELL_PATTERN, line, constants.SPELL_REPLACE_REGEX, constants.WOWHEAD_SPELL_URL)
+    if phonetic:
+        return replace_pattern('Mutating(spell_patterns): ', constants.SPELL_PHONETIC_PATTERN, line, constants.SPELL_PHONETIC_REPLACE_REGEX, None)
+    else:
+        return replace_pattern('Mutating(spell_patterns): ', constants.SPELL_PATTERN, line, constants.SPELL_REPLACE_REGEX, constants.WOWHEAD_SPELL_URL)
 
 
-def replace_talent_patterns(line):
+def replace_talent_patterns(line: str, phonetic: bool) -> str:
     """Replaces World of Warcraft enchant patterns with wowhead.com urls"""
-    return replace_pattern('Mutating(talent_patterns): ', constants.TALENT_PATTERN, line, constants.TALENT_REPLACE_REGEX, None)
+    if phonetic:
+        return replace_pattern('Mutating(talent_patterns): ', constants.TALENT_PHONETIC_PATTERN, line, constants.TALENT_PHONETIC_REPLACE_REGEX, None)
+    else:
+        return replace_pattern('Mutating(talent_patterns): ', constants.TALENT_PATTERN, line, constants.TALENT_REPLACE_REGEX, None)
 
 
 def replace_pattern(debug, pattern, line, regex, url):
